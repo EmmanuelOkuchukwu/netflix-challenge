c8816816342b9a84470677ec2420ec76b84844f3
emmanz95 <emmanza2@gmail.com>
2021-02-04 13:35:51 +0000

    New Changes

--- src/Banner.js
+++ src/Banner.js
@@ -35,21 +35,13 @@ function Banner() {
         }}
         >
         <div className="banner__contents">
-            <h1 className="banner__title">Movie Name</h1>
+            <h1 className="banner__title">{movie?.original_name}</h1>
             <div className="banner__buttons">
                 <button className="banner__button">Play</button>
                 <button className="banner__button">My List</button>
             </div>
             <h1 className="banner__description">
-            {truncate(
-               `This is a test description
-            This is a test descriptionThis is a test description
-            This is a test descriptionThis is a test description
-            This is a test descriptionThis is a test description
-            This is a test descriptionThis is a test description
-            This is a test descriptionThis is a test description`, 150
-            )}
-
+                {truncate(movie?.overview, 150)}
             </h1>
         </div>
 
--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import './Nav.css'
 
 function Nav() {
-    const [show, handleShow] = useState(false);
+    const [show, handleShow] = useState(true);
     const transitionNavBar = () => {
         if(window.scrollY > 100) {
             handleShow(true);
@@ -15,14 +15,14 @@ function Nav() {
         return () => window.removeEventListener("scroll", transitionNavBar)
     }, [])
     return (
-        <div className={`nav ${show && "nav__black"}`}>
+        <div className={`nav ${show && 'nav__black'}`}>
             <div className="nav__contents">
-                <img 
+                <img
                     className="nav__logo"
-                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                     alt=""
                 />
-                <img 
+                <img
                     className="nav__avatar"
                     src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
                     alt=""
